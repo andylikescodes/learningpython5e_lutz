f1caaab5e75b6e99becf5a25ef468c2e953a19ac
Dehua Liang <andylikescodes@gmail.com>
2017-04-13 09:59:32 -0700

    added chapter 3 reading notes

---
+++ chapter3/main.py
@@ -0,0 +1,7 @@
+import script
+
+#show all variables in script
+dir(script)
+
+# read the script.py file in this section
+exec(open('script.py').read())
---
+++ chapter3/script.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import sys
+print (sys.platform)
+print (2**100)
+x = 'Spam!'
+print (x*8)
+
+def dummy():
+    pass
+
+## Debug: pdb commend-line debugger & python -i
